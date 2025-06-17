a2771f36fe48ed8a07887a3074af3b1649314bbb
Tamil Selvan <tamildharshini007@gmail.com>
2025-06-17 20:10:17 +0530

    modify the file

---
+++ script.js
@@ -0,0 +1,28 @@
+// var displayScreen = document.getElementById("screen");
+
+// function displayAppend(input)
+// {
+//     displayScreen.value += input;
+// }
+
+let displayScreen = document.getElementById("screen");
+
+function displayAppend(input) {
+    displayScreen.value += input;
+}
+
+function clearAll() {
+    displayScreen.value = "";
+}
+
+function clearLast() {
+    displayScreen.value = displayScreen.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        displayScreen.value = eval(displayScreen.value);
+    } catch {
+        displayScreen.value = "Error";
+    }
+}
